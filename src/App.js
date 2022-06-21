0631755ad532fdcf773f7b8052dc40237478a914
Joe Gencarelli <jp.genc@gmail.com>
2022-06-21 12:46:42 -0400

    PI test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import {useEffect} from 'react'
 import './App.css';
 
 function App() {
+  useEffect (()=>{
+    axios ({
+      url:`https://api.currentsapi.services/v1/latest-news`,
+      
+              
+      params:{
+        apiKey:`WYB2g_IF3u2aTOW2WjDYQeTFuJl84VJ04t4jq7941IFdVNfv`,
+        category:'business'
+      }
+    })
+    .then ((response)=>{
+      console.log(response.data.news);
+    })
+    
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+ 
+  </>
   );
 }
 
