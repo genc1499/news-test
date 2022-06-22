10158b73978e19131de52a0dd540d545d55478ad
Joe Gencarelli <jp.genc@gmail.com>
2022-06-22 02:40:46 -0400

    test two

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import './App.css';
 function App() {
   useEffect (()=>{
     axios ({
-      url:`https://api.currentsapi.services/v1/latest-news`,
-      
-              
-      params:{
-        apiKey:`WYB2g_IF3u2aTOW2WjDYQeTFuJl84VJ04t4jq7941IFdVNfv`,
-        category:'business'
-      }
-    })
-    .then ((response)=>{
-      console.log(response.data.news);
-    })
     
-  }, [])
+            url:`http://api.weatherapi.com/v1/forecast.json`,
+            params:{
+                key:`76b75a7ececc4d1c86341505222206`,
+                q:`Toronto`,
+                days:'5'
+            }
+          })
+      
+          .then((response)=>{
+          console.log(response.data.forecast.forecastday);
+          setWeather(response.data.forecast.forecastday);
+          
+           
+          })
+         .catch(error => {
+            console.log(error.response.data.error)
+         })
+        
+      },[])
+      
   return (
   <>
  
